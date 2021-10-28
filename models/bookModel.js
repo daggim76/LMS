ed6ae06f34bacd9d12da58e80378e6a8d3ffe28d
Daggim Hailu <daggim@kali>
2021-10-28 02:23:55 -0400

    model updated

--- models/bookModel.js
+++ models/bookModel.js
@@ -8,56 +8,41 @@ const bookSchema = new mongoose.Schema({
     required: [true, 'A book must have a name'],
     trim: true,
     maxlength: [40, 'A book name must have less or equal then 40 characters'],
-    minlength: [2, 'A book name must have more or equal then 2 characters'],
+    minlength: [2, 'A book name must have more or equal then 2 characters']
     // validate: [validator.isAlpha, 'Book name must only contain characters']
   },
-  slug: String,
 
   department: {
     type: String,
     required: [true, 'A book must have a department'],
     enum: {
       values: ['Computer Science', 'Marketing', 'Accounting', 'Managment'],
-      message: 'Department is either: Marketing, Accounting, Managment',
-    },
+      message:
+        'Department is either: Computer Science, Marketing, Accounting, Managment'
+    }
   },
   ISBN: {
-    type: Number,
-    min: [8, 'Rating must be above 1.0'],
-    max: [15, 'Rating must be below 5.0'],
+    type: String,
+    max: [15, 'Rating must be below 5.0']
   },
   quantity: {
     type: Number,
-    default: 1,
+    default: 1
   },
 
-  edition: {
+  year: {
     type: Number,
-    default: 1,
+    min: [4, 'Please Enter a year in a correct fromat, at list 4 characters']
   },
 
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A book must have a description'],
-  },
   author: {
     type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-  },
-  images: [String],
-  year: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
+    trim: true
+  }
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
-bookSchema.pre('save', function (next) {
+bookSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
@@ -81,13 +66,13 @@ bookSchema.pre('save', function (next) {
 //   next();
 // });
 
-bookSchema.post(/^find/, function (docs, next) {
+bookSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
 // AGGREGATION MIDDLEWARE
-bookSchema.pre('aggregate', function (next) {
+bookSchema.pre('aggregate', function(next) {
   this.pipeline().unshift();
 
   console.log(this.pipeline());
